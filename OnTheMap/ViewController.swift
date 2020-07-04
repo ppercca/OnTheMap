c93dbfbd8115d0706c5efc3960b7f1c7f6fa5669
Paul Cristian Percca Julca <pcristian.percca.julca@gmail.com>
2020-07-03 23:39:14 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Paul Cristian Percca Julca on 7/3/20.
+//  Copyright © 2020 Innovatrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
